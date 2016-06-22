00cc12c0ab56fcbd392f1034110f688e1e87de8c
Diana Duong <dduong013@gmail.com>
2016-06-22 15:36:07 -0500

    updated to use var vm

--- js/script.js
+++ js/script.js
@@ -1,75 +1,76 @@
 var app = angular.module('myApp', ['ngMessages'])
 	app.controller('CalculatorController', function(){
 
-    this.mealPrice = 0;
-    this.mealTax = 0;
-    this.mealTip = 0;
-    this.earningsTipTotal = 0;
-    this.customerSubtotal = 0;
-    this.customerTotalTip = 0;
-    this.customerTotalCharge = 0;
-    this.mealCount = 0;
-    this.avgTip = 0;
+    var vm = this;
+
+    vm.mealPrice = 0;
+    vm.mealTax = 0;
+    vm.mealTip = 0;
+    vm.earningsTipTotal = 0;
+    vm.customerSubtotal = 0;
+    vm.customerTotalTip = 0;
+    vm.customerTotalCharge = 0;
+    vm.mealCount = 0;
+    vm.avgTip = 0;
 
     //submitted forms empty array
-   	this.submittedMealData = [];
+   	vm.submittedMealData = [];
 
     // function to submit the form after all validation has occurred
-    this.submitForm = function(isValid){
+    vm.submitForm = function(isValid){
       //if form is valid
       if (isValid){
 
-        this.mealCount++;
+        vm.mealCount++;
         //push users input into the array
-        this.submittedMealData.push()
+        vm.submittedMealData.push()
          
         //multiplying price and tip to get a dollar amount
-        this.customerTotalTip = this.mealPrice * (this.mealTip / 100);
+        vm.customerTotalTip = vm.mealPrice * (vm.mealTip / 100);
 
         //multiplying price to tax to get the totalTax and then add to price
-        this.customerSubtotal = (this.mealPrice * (this.mealTax / 100)) + this.mealPrice
+        vm.customerSubtotal = (vm.mealPrice * (vm.mealTax / 100)) + vm.mealPrice
 
         // multiplyin the subtotal to the tip to get the totalTip and add to the subtotal to get the customerTotalCharge
-        this.customerTotalCharge = this.customerSubtotal + this.customerTotalTip;
+        vm.customerTotalCharge = vm.customerSubtotal + vm.customerTotalTip;
 
-        this.tipTotal()
-        this.averageTip()
-        
+        vm.tipTotal()
+        vm.averageTip()
       
-        this.mealPrice = 0;
-        this.mealTax = 0;
-        this.mealTip = 0;
+        vm.mealPrice = 0;
+        vm.mealTax = 0;
+        vm.mealTip = 0;
       }
     }
   
     //function to calculate the average tip per meal count
-    this.averageTip = function(){
-      this.avgTip = this.earningsTipTotal / this.mealCount;
+    vm.averageTip = function(){
+      vm.avgTip = vm.earningsTipTotal / vm.mealCount;
     }
 
     //function to calulate overall total custoer tips
-    this.tipTotal = function(){
-      this.earningsTipTotal += this.customerTotalTip;
+    vm.tipTotal = function(){
+      vm.earningsTipTotal += vm.customerTotalTip;
     }
 
     // function to cancel meal details input
-    this.cancelForm = function(){
-      this.mealPrice = 0;
-      this.mealTax = 0;
-      this.mealTip = 0;
+    vm.cancelForm = function(){
+      vm.mealPrice = 0;
+      vm.mealTax = 0;
+      vm.mealTip = 0;
     }
 
     // function to reset all fields
-    this.resetForm = function(){
-      this.mealPrice = 0;
-      this.mealTax = 0;
-      this.mealTip = 0;
-      this.earningsTipTotal = 0;
-      this.customerSubtotal = 0;
-      this.customerTotalTip = 0;
-      this.customerTotalCharge = 0;
-      this.mealCount = 0;
-      this.avgTip = 0;
+    vm.resetForm = function(){
+      vm.mealPrice = 0;
+      vm.mealTax = 0;
+      vm.mealTip = 0;
+      vm.earningsTipTotal = 0;
+      vm.customerSubtotal = 0;
+      vm.customerTotalTip = 0;
+      vm.customerTotalCharge = 0;
+      vm.mealCount = 0;
+      vm.avgTip = 0;
     }
 
 	})
